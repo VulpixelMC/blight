f55ff806b17b05c4b5fec7899459e6d56c329664
sylv256 <contact@sylv.gay>
2025-06-18 18:09:31 -0400

    feat: Hexgrid interpretation

--- src/main/java/gay/sylv/blight/api/vm/Hexgrid.java
+++ src/main/java/gay/sylv/blight/api/vm/Hexgrid.java
@@ -1,9 +1,43 @@
 package gay.sylv.blight.api.vm;
 
+import org.jetbrains.annotations.NotNull;
+
 // TODO: Design and implement Hexgrid API
 public class Hexgrid {
+	private boolean textMode = true;
+	private @NotNull String content;
+	private boolean dirty;
+	private Nature[] cachedNatures = new Nature[0];
+
+	public Hexgrid(@NotNull String content) {
+		this.content = content;
+	}
+
+	public Hexgrid() {
+		this.content = "";
+	}
+
+	public @NotNull String getContent() {
+		return content;
+	}
+
+	public void setContent(@NotNull String content) {
+		this.content = content;
+		this.dirty = true;
+	}
+
 	public Nature[] interpret() {
+		if (!dirty && cachedNatures != null) return cachedNatures;
+
 		// Interpret Hexgrid as array of Natures
-		return null;
+		final String parsed = content
+				.replaceAll("#.*", "")
+				.replaceAll("\n", "");
+		this.cachedNatures = new Nature[parsed.length()];
+		for (int i = 0; i < parsed.length(); i++) {
+			cachedNatures[i] = Nature.fromSymbol(parsed.charAt(i));
+		}
+
+		return cachedNatures;
 	}
 }
--- src/main/java/gay/sylv/blight/api/vm/Nature.java
+++ src/main/java/gay/sylv/blight/api/vm/Nature.java
@@ -1,5 +1,9 @@
 package gay.sylv.blight.api.vm;
 
+import it.unimi.dsi.fastutil.chars.Char2IntArrayMap;
+import it.unimi.dsi.fastutil.chars.Char2IntMap;
+import it.unimi.dsi.fastutil.chars.Char2IntOpenHashMap;
+
 /**
  * <h1>Nature</h1>
  * Defines the characteristics of Natures used in constructing Spells and Hexes within the {@code gay.sylv.blight.api.vm} package.
@@ -29,7 +33,14 @@ public enum Nature {
 	Grouping('G'),
 	Semantic('Z'),;
 
+	private static final Char2IntMap lookup = new Char2IntOpenHashMap();
 	private final char symbol;
+
+	static {
+		for (Nature n : Nature.values()) {
+			lookup.put(n.symbol, n.ordinal());
+		}
+	}
 
 	Nature(char symbol) {
 		this.symbol = symbol;
@@ -40,5 +51,9 @@ public enum Nature {
 
 	public char getSymbol() {
 		return symbol;
+	}
+
+	public static Nature fromSymbol(char symbol) {
+		return Nature.values()[lookup.get(symbol)];
 	}
 }
