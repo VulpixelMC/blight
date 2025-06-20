d265c90a94ff83b3d556618a6176edfa608e53c4
sylv256 <contact@sylv.gay>
2025-06-19 21:19:35 -0400

    feat: setUniform with Vec3

--- src/client/java/gay/sylv/blight/client/api/render/blight3d/BlightRenderPass.java
+++ src/client/java/gay/sylv/blight/client/api/render/blight3d/BlightRenderPass.java
@@ -2,6 +2,7 @@ package gay.sylv.blight.client.api.render.blight3d;
 
 import com.mojang.blaze3d.opengl.GlRenderPass;
 import com.mojang.blaze3d.systems.RenderPass;
+import net.minecraft.world.phys.Vec3;
 
 /**
  * A series of improvements to {@link RenderPass}.
@@ -12,4 +13,8 @@ public interface BlightRenderPass extends RenderPass {
 	default void blight$enableStencil() {}
 
 	default void blight$disableStencil() {}
+
+	default void setUniform(String name, Vec3 value) {
+		setUniform(name, (float) value.x, (float) value.y, (float) value.z);
+	}
 }
